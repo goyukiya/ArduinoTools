7cd8163789ac3a4afbbdc0950b05bc28ece2deba
yuki <yukigoya@gmail.com>
2021-10-15 17:25:41 +0900

    multiple pulse output type added

--- PulseGenerator/pulseGenerator.ino
+++ PulseGenerator/pulseGenerator.ino
@@ -1,47 +1,69 @@
+// output pin
 const int OUTPUT_PIN=2;
-const int INPUT_PULSE_PIN=8;
+// input pins
+const int INPUT_PULSE0_PIN=7;
+const int INPUT_PULSE1_PIN=8;
+const int INPUT_PULSE2_PIN=9;
 const int INPUT_STATIC_PIN=10;
 
-double pulseFrequency; // in Hz
-double pulseDutyCycle; // in %
-unsigned long highDuration; // in microseconds
-unsigned long lowDuration; // in microseconds
+// pulse duty cycle in %
+const int PULSE_DUTY = 20;
+// pulse frequencies in Hz 
+const int PULSE0_FREQ = 20;
+const int PULSE1_FREQ = 10;
+const int PULSE2_FREQ = 4;
+ 
+// pulse low/high durations in microseconds
+unsigned long lowDurations[3];
+unsigned long highDurations[3];
 
 unsigned long previousTime=0;
 
-void setup() {
+void setup()
+{
   // set pins
   pinMode(OUTPUT_PIN, OUTPUT);   
-  pinMode(INPUT_PULSE_PIN, INPUT);
+  pinMode(INPUT_PULSE0_PIN, INPUT);
+  pinMode(INPUT_PULSE1_PIN, INPUT);
+  pinMode(INPUT_PULSE2_PIN, INPUT);
   pinMode(INPUT_STATIC_PIN, INPUT);
-  
-  // define frequency and duty cycle
-  pulseFrequency= 20; // Hz
-  pulseDutyCycle= 20; // %
-  // compute HIGH and LOW durations   
-  double pulsePeriod= 1000.0 * 1000.0/pulseFrequency; // microseconds
-  highDuration=(unsigned long)(pulsePeriod * pulseDutyCycle/100);
-  lowDuration=(unsigned long)(pulsePeriod - highDuration);
 
+  // compute HIGH and LOW durations
+  // PULSE0
+  double pulsePeriod= 1000.0 * 1000.0/PULSE0_FREQ; // microseconds
+  highDurations[0]=(unsigned long)(pulsePeriod * PULSE_DUTY/100);
+  lowDurations[0]=(unsigned long)(pulsePeriod - highDurations[0]);
+  // PULSE1
+  pulsePeriod= 1000.0 * 1000.0/PULSE1_FREQ;
+  highDurations[1]=(unsigned long)(pulsePeriod * PULSE_DUTY/100);
+  lowDurations[1]=(unsigned long)(pulsePeriod - highDurations[1]);
+  // PULSE2
+  pulsePeriod= 1000.0 * 1000.0/PULSE2_FREQ;
+  highDurations[2]=(unsigned long)(pulsePeriod * PULSE_DUTY/100);
+  lowDurations[2]=(unsigned long)(pulsePeriod - highDurations[2]);
+  
   // initial status
   digitalWrite(OUTPUT_PIN, LOW);
 }
 
-void loop(){
-  if(digitalRead(INPUT_PULSE_PIN)==HIGH){ // pulse output
-    // one pulse
-    digitalWrite(OUTPUT_PIN, HIGH);
-    // wait for highDuration
-    previousTime = micros();
-    while(!(micros()-previousTime > highDuration)){} 
-    digitalWrite(OUTPUT_PIN, LOW);
-    // wait for lowDuration
-    previousTime = micros();
-    while(!(micros()-previousTime > lowDuration)){}  
-  }else if(digitalRead(INPUT_STATIC_PIN)==HIGH){ // static output
-    digitalWrite(OUTPUT_PIN, HIGH);
-  }
-  else{ // zero
-    digitalWrite(OUTPUT_PIN, LOW);
-  }
+void outputOnePulse(int idx)
+{
+  // one pulse
+  digitalWrite(OUTPUT_PIN, HIGH);
+  // wait for highDuration
+  previousTime = micros();
+  while(!(micros()-previousTime > highDurations[idx])){} 
+  digitalWrite(OUTPUT_PIN, LOW);
+  // wait for lowDuration
+  previousTime = micros();
+  while(!(micros()-previousTime > lowDurations[idx])){}  
+}
+
+void loop()
+{
+  if(digitalRead(INPUT_PULSE0_PIN)==HIGH) outputOnePulse(0); // pulse 0
+  else if (digitalRead(INPUT_PULSE1_PIN)==HIGH) outputOnePulse(1); // pulse 1
+  else if(digitalRead(INPUT_PULSE2_PIN)==HIGH) outputOnePulse(2); // pulse 2
+  else if(digitalRead(INPUT_STATIC_PIN)==HIGH) digitalWrite(OUTPUT_PIN, HIGH); // static
+  else digitalWrite(OUTPUT_PIN, LOW); // none
 }
