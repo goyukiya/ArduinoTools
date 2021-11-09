5caa3df7977f2c167715f6e8926bb228ff269f46
GOYA Yuki <46738403+goyukiya@users.noreply.github.com>
2021-11-09 11:03:45 +0900

    Update pulseGenerator.ino

--- PulseGenerator/pulseGenerator.ino
+++ PulseGenerator/pulseGenerator.ino
@@ -4,20 +4,23 @@ const int OUTPUT_PIN=2;
 const int INPUT_PULSE0_PIN=7;
 const int INPUT_PULSE1_PIN=8;
 const int INPUT_PULSE2_PIN=9;
+const int INPUT_PULSE3_PIN=11;
 const int INPUT_STATIC_PIN=10;
 
 // pulse High duration in microseconds
-const int PULSE0_DUR= 5000;
-const int PULSE1_DUR = 5000;
-const int PULSE2_DUR = 5000;
+const long PULSE0_DUR= 5000;
+const long PULSE1_DUR = 5000;
+const long PULSE2_DUR = 5000;
+const long PULSE3_DUR = 1000000;
 // pulse frequencies in Hz 
-const int PULSE0_FREQ = 20;
-const int PULSE1_FREQ = 10;
-const int PULSE2_FREQ = 4;
+const float PULSE0_FREQ = 20.0;
+const float PULSE1_FREQ = 10.0;
+const float PULSE2_FREQ = 4.0;
+const float PULSE3_FREQ = 0.333;
  
 // pulse low/high durations in microseconds
-unsigned long lowDurations[3];
-unsigned long highDurations[3];
+unsigned long lowDurations[4];
+unsigned long highDurations[4];
 
 unsigned long previousTime=0;
 
@@ -42,7 +45,11 @@ void setup()
   // PULSE2
   pulsePeriod= 1000.0 * 1000.0/PULSE2_FREQ;
   highDurations[2]= (unsigned long) ((PULSE2_DUR >= pulsePeriod)? pulsePeriod/2 : PULSE2_DUR);
-  lowDurations[2]=(unsigned long) (pulsePeriod - highDurations[2]);
+  lowDurations[2]= (unsigned long) (pulsePeriod - highDurations[2]);
+  // PULSE3
+  pulsePeriod= 1000.0 * 1000.0/PULSE3_FREQ;
+  highDurations[3]= (unsigned long) ((PULSE3_DUR >= pulsePeriod)? pulsePeriod/2 : PULSE3_DUR);
+  lowDurations[3]= (unsigned long) (pulsePeriod - highDurations[3]);
   
   // initial status
   digitalWrite(OUTPUT_PIN, LOW);
@@ -66,6 +73,7 @@ void loop()
   if(digitalRead(INPUT_PULSE0_PIN)==HIGH) outputOnePulse(0); // pulse 0
   else if (digitalRead(INPUT_PULSE1_PIN)==HIGH) outputOnePulse(1); // pulse 1
   else if(digitalRead(INPUT_PULSE2_PIN)==HIGH) outputOnePulse(2); // pulse 2
-  else if(digitalRead(INPUT_STATIC_PIN)==HIGH) digitalWrite(OUTPUT_PIN, HIGH); // static
+  else if(digitalRead(INPUT_PULSE3_PIN)==HIGH) outputOnePulse(3); // pulse 3
+  else if(digitalRead(INPUT_STATIC_PIN)==HIGH) digitalWrite(OUTPUT_PIN, HIGH); // static  
   else digitalWrite(OUTPUT_PIN, LOW); // none
 }
